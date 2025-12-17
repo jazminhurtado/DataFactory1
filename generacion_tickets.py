414e58fcdda22f790cbd587a55e3e04de87aec92
jazminhurtado <jazhurgraster@gmail.com>
2025-12-17 09:35:55 -0500

    Update generacion_tickets.py

--- generacion_tickets.py
+++ generacion_tickets.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import random
 
 # --- CONFIGURACIÓN ---
 st.set_page_config(page_title="Minería de Procesos", layout="wide")
